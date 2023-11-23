c27bd98b11e7bf96c6882f1c06e804a43b604181
drizzle021 <gabrielabraham004@gmail.com>
2023-11-23 12:51:52 +0100

    GUI..
    LETS FCKIGOGGOO

--- Client.py
+++ Client.py
@@ -1,5 +1,37 @@
-import socket
+import socket as s
 import threading
-import sys
+import time
 
-server = ()
+class Client:
+    def __init__(self, server: tuple):
+        self.socket = s.socket(s.AF_INET, s.SOCK_DGRAM)
+        self.serverIP, self.serverPort = server
+        self.fragmentSize = 512
+        self.connected = False
+        self.data = None
+        """t1 = threading.Thread(target=self.listen)
+        t1.start()
+        while not connected:
+            self.sendMessage("init")
+            if self.data == "ack":
+                self.connected = True"""
+        self.connected = True
+        tKeepAlive = threading.Thread(target=self.keepAlive,daemon=True)
+        tKeepAlive.start()
+
+
+
+    def sendMessage(self, message):
+        self.socket.sendto(bytes(message),(self.serverIP,self.serverPort))
+
+    def listen(self):
+        self.data = self.socket.recvfrom(1024)
+        time.sleep(0.5)
+
+    def keepAlive(self):
+        while self.connected:
+            print("alive")
+            # self.socket.sendto(bytes("alive"),(self.serverIP,self.serverPort))
+            print(threading.enumerate())
+            time.sleep(5)
+
--- Server.py
+++ Server.py
@@ -1,29 +1,8 @@
-import socket
+import socket as s
 
-port = 50002
+class Server:
+    def __init__(self, ip, port):
+        self.socket = s.socket(s.AF_INET, s.SOCK_DGRAM)
+        self.socket.bind((ip,port))
 
-sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sck.bind(("0.0.0.0", 55555))
-
-while True:
-    clients = []
-
-    while True:
-        data, address = sck.recvfrom(128)
-
-        print(f"connection from {address}")
-        clients.append(address)
-
-        sck.sendto(b"ready", address)
-
-        if len(clients) == 2:
-            print("got 2 clients, sending details to each")
-            break
-    c1 = clients.pop()
-    c1_address, c1_port = c1
-    c2 = clients.pop()
-    c2_address, c2_port = c2
-
-    sck.sendto(f"{c1_address} {c1_port} {port}".encode(), c1)
-    sck.sendto(f"{c2_address} {c2_port} {port}".encode(), c2)
-
+    
